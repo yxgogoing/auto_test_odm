fb5e7b587896c39ee93ffc7c482257aca9abcedf
yangxiao <yxgogoing@126.com>
2022-03-16 22:38:33 +0800

    123

---
+++ supplier_service/login_service/__init__.py
@@ -0,0 +1,17 @@
+# from supplier_service.login_service.api.login_service.user_info import user
+#
+# def get_auth():
+#     from supplier_service.login_service.api.login_service.login_api import Login
+#     login_api_obj = Login().send_request()
+#     return_data = login_api_obj.resp.data
+#     return return_data.tokenHead + ' ' + return_data.token
+
+
+# # from supplier_service.login_service.api.login_service.user_info import user
+# from supplier_service.login_service.api.login_service.user_info import UserInfo
+# user = UserInfo(mobile='13430212979', password='chaowen666').login()
+
+
+
+
+
